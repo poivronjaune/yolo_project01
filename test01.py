848fba0bc3a7a8198e536162a19629315fc73afa
PoivronJaune <poivronjaune@gmail.com>
2025-03-24 16:40:50 -0400

    Detection of cars in test02.py

--- test01.py
+++ test01.py
@@ -2,9 +2,9 @@ from ultralytics import YOLO
 
 
 # Run inference on an image
-# yolo11n.pt, yolo11s.pt, yolo11m.pt, yolo11l.pt, yolo11x.pt
-model = YOLO('yolo11x.pt')  # Load model
-results = model('.\images\image01.jpg') 
+# yolo11n.pt, yolo11s.pt, yolo11m.pt, yolo11l.pt, yolo11x.pt, yolo12n.pt
+model = YOLO('yolo12n.pt')  # Load model
+results = model('.\images\image02.jpg') 
 
 # Process results list
 for result in results:
